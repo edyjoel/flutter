f62876be67ace223eeb72f018d865483e752c68a
Edy Joel <edyxicon@gmail.com>
2025-08-02 17:29:47 -0600

    Implement video posts data source and repository with initial methods

--- toktik/lib/presentation/providers/discover_provider.dart
+++ toktik/lib/presentation/providers/discover_provider.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
-import 'package:toktik/infraestructure/models/local_video_model.dart';
-import 'package:toktik/shared/data/local_video_posts.dart';
+import 'package:toktik/domain/repositories/video_posts_repository.dart';
 
 class DiscoverProvider extends ChangeNotifier {
-// TODO: Repository, DataSource
+  late final VideoPostsRepository videoPostsRepository;
 
   bool initialLoading = true;
   List<VideoPost> videos = [];
 
+  DiscoverProvider({required this.videoPostsRepository});
+
   Future<void> loadNextPage() async {
     // await Future.delayed(const Duration(seconds: 2));
 
-    final List<VideoPost> newVideos = videoPosts
-        .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
-        .toList();
+    // final List<VideoPost> newVideos = videoPosts
+    //     .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
+    //     .toList();
+
+    final newVideos = await videoPostsRepository.getTrendingVideosByPage(1);
 
     videos.addAll(newVideos);
     initialLoading = false;
