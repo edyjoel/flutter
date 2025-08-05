81238e2a1a1a75615ad20de49551f7083855a4f7
Edy Joel <edyxicon@gmail.com>
2025-08-04 23:05:56 -0600

    GoRouter configured

--- widgets_app/lib/presentation/screens/home/home_screen.dart
+++ widgets_app/lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
@@ -52,7 +53,8 @@ class _CustomListTile extends StatelessWidget {
       onTap: () {
         // Navigator.of(context).push(
         //     MaterialPageRoute(builder: (context) => const ButtonsScreen()));
-        Navigator.pushNamed(context, menuItem.link);
+        // Navigator.pushNamed(context, menuItem.link);
+        context.push(menuItem.link);
       },
     );
   }
